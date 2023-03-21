ac6ea31a9662d2a3b69cc75512ef671a94d20870
Gergana Georgieva <gerganagergana1@gmail.com>
2023-03-21 16:59:07 +0200

    Ex 2

--- P02_FootballBetting/Data/Models/Team.cs
+++ P02_FootballBetting/Data/Models/Team.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,10 @@ namespace P02_FootballBetting.Data.Models
 {
     public class Team
     {
+        [Key]
+        public int TeamId { get; set; }
+
+        public string? Name { get; set; }
+        public string? LogoUrl { get; set; }
     }
 }
